217e549fcdb74753cf57d0c0e7e01e4774e2fd8d
LZS <2802898747@qq.com>
2023-10-10 21:22:02 +0800

    finished C01 code work

---
+++ homework/C01W04/code/C01W04_1.py
@@ -0,0 +1,4 @@
+# Author: LZS
+# CreateTime: 2023/10/10  20:24
+# FileName: C01W04_1
+# Python Script
---
+++ homework/C02W01/code/C02W01_1.py
@@ -0,0 +1,4 @@
+# Author: LZS
+# CreateTime: 2023/10/10  21:17
+# FileName: C02W01_1
+# Python Script
